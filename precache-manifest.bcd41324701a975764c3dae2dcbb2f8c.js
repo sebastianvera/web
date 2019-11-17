573347ac1a04c75e1a1744c5fc8e84933ab47f34
Sebastian Vera <u.verainf@gmail.com>
2019-11-17 19:24:44 +0100

    Updates

---
+++ precache-manifest.bcd41324701a975764c3dae2dcbb2f8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1100af0feec60373283fc0f6a7125c",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "86456023cf22d1c30049",
+    "url": "/web/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "19d99b06ff7846f58a71",
+    "url": "/web/static/js/2.adc3e57d.chunk.js"
+  },
+  {
+    "revision": "86456023cf22d1c30049",
+    "url": "/web/static/js/main.5081303e.chunk.js"
+  },
+  {
+    "revision": "20b29f96bef68c827736",
+    "url": "/web/static/js/runtime-main.9f7f59e7.js"
+  },
+  {
+    "revision": "5fa736e057e3e8d23388ae2f17fef3a4",
+    "url": "/web/static/media/github.5fa736e0.svg"
+  },
+  {
+    "revision": "1a835eb0decd99c38e6cc4b3bb598358",
+    "url": "/web/static/media/profile.1a835eb0.jpg"
+  },
+  {
+    "revision": "f322a147dbbcee6eec1f1ab715bfd890",
+    "url": "/web/static/media/twitter.f322a147.svg"
+  }
+]);
