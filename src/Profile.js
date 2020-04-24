0e7b8628768de5b41aef9dea5029e1a355570c10
Sebastian Vera <u.verainf@gmail.com>
2020-04-24 02:35:40 +0200

    feat: add link to my personal blog and prettify file

--- src/Profile.js
+++ src/Profile.js
@@ -5,10 +5,10 @@ import pic from './profile.jpg';
 
 const defaultSize = 100;
 const RoundedImage = styled.img`
-  box-shadow: 0 0 8px rgba(0, 0, 0, .6);
-  height: ${props => props.size || defaultSize}px;
-  width: ${props => props.size || defaultSize}px;
-  border-radius: ${props => (props.size || defaultSize) / 2}px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.6);
+  height: ${(props) => props.size || defaultSize}px;
+  width: ${(props) => props.size || defaultSize}px;
+  border-radius: ${(props) => (props.size || defaultSize) / 2}px;
   user-drag: none;
   user-select: none;
 `;
@@ -19,34 +19,48 @@ const Link = styled.a`
 `;
 
 const Emoji = styled.span`
-  padding: 0 2px;
   font-size: 1em;
+  text-align: center;
 `;
 
 const Title = styled.h1`
   text-align: center;
-  color: #FAFBFB;
+  color: #fafbfb;
   font-family: 'Open Sans', sans-serif;
+`;
+
+const Footer = styled.p`
+  text-align: center;
+  font-size: 1em;
+  color: #fafbfb;
+  font-family: 'Raleway', sans-serif;
+  font-style: italic;
+  margin-top: 2rem;
 `;
 
 const Text = styled.p`
   text-align: center;
-  color: #FAFBFB;
+  color: #fafbfb;
   font-family: 'Raleway', sans-serif;
 `;
 
-const Profile = () =>
+const Profile = () => (
   <div>
     <RoundedImage src={pic} size={200} alt="Sebastian Vera picture" />
     <Title>Sebastián Vera</Title>
     <Text>
       Fullstack Developer that{' '}
-      <Emoji role="img" aria-label="love">💚</Emoji>
-      {' '}
-      react, golang and nodejs. I'm currently working as a Full Stack Developer at
-      {' '}
-      <Link href="https://twitter.com/Cision">@Cision</Link>.
+      <Emoji role="img" aria-label="love">
+        💚
+      </Emoji>{' '}
+      react, golang and nodejs. I'm currently working as a Full Stack Developer
+      at <Link href="https://twitter.com/Cision">@Cision</Link>.
     </Text>
-  </div>;
+    <Footer>
+      Visit my blog at{' '}
+      <Link href="https://blog.apipath.io">blog.apipath.io</Link>
+    </Footer>
+  </div>
+);
 
 export default Profile;
